24e3941ec3d13f03593b28b53a59da7ee39afb21
tgerhardt <tgerhardt91@gmail.com>
2020-02-06 21:30:21 -0600

    Added bones of mapbox code

---
+++ app/src/main/java/com/example/bloccompass/timberTrees/NotLoggingTree.java
@@ -0,0 +1,9 @@
+package com.example.bloccompass.timberTrees;
+
+import timber.log.Timber;
+
+public class NotLoggingTree extends Timber.Tree {
+    @Override
+    protected void log(final int priority, final String tag, final String message, final Throwable throwable) {
+    }
+}
